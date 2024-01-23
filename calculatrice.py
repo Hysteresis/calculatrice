af9df2f80b019d642c0a929ba50990e730d3af65
Pierre Marboutin <piballz@gmail.com>
2024-01-23 16:13:01 +0100

    added addition

--- calculatrice.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-class Calculatrice:
-    def __init__(self, master):
-        self.master = master
-        master.title('Calculatrice')
-        self.display = tk.Entry(master, bg="#98FB98")
-        self.display.grid(row=0, column=0, columnspan=3, )
-        for i in range(10):
-            tk.Button(master, text=str(i), command=lambda)
--- main.py
+++ main.py
@@ -1,8 +1,48 @@
 import tkinter as tk
-from calculatrice import Calculatrice
+
+result = []
+
+
+def on_key_press(event):
+    addition = 0
+    current_text = entry.get()
+    if event.char.isdigit() or event.char in "+-*/":
+        result.append(current_text)
+        print(result)
+    elif event.char.isalpha():
+        pass
+    elif event.keysym == "BackSpace":
+        clear_char()
+    elif event.keysym == "Return":
+        print(result[-1])
+        arr = result[-1].split("+")
+        for element in arr:
+            addition += float(element)
+        print(addition)
+        entry_result.delete(0, tk.END)
+        entry_result.insert(tk.END, str(addition))
+        return addition
+    else:
+        pass
+
+
+def clear_char():
+    current_text = entry.get()
+    if current_text:
+        entry.delete(len(current_text) - 1)
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = Calculatrice(root)
+
+    display = tk.Entry(root, bg="#98FB98")
+    display.grid(row=0, column=0, columnspan=3)
+
+    root.bind('<Key>', on_key_press)
+
+    entry = display
+
+    entry_result = tk.Entry(root, bg="#98FB98")
+    entry_result.grid(row=1, column=0, columnspan=3)
+
     root.mainloop()
