80a813d01d32b6627d15aacd0d0a7c8aa7199e62
Pierre Marboutin <piballz@gmail.com>
2024-01-24 10:54:35 +0100

    added test file xlsx

--- README.md
+++ README.md
@@ -1,23 +1,40 @@
-# calculatrice
+# Projet "**Calculatrice**"
+- version de python : 3.10 ou supérieure
+- créer un fichier "**calcul_history.csv**" dans le dossier racine
+- dans le dossier "test" ouvrir le fichier de test "cahier_de_tests_calculatrice_Pierre.xlsx"
+- vérifier que les tests fonctionnent
 
-## Durée :
-7h
 
 ## Ennoncé
 En utilisant **Tkinter**, votre mission sera de réaliser une **calculatrice**.
 
 La contrainte à ce projet, est que la calculatrice ne possède qu’au **maximum trois boutons** :
-- Bouton 'C' : effacer
-- Bouton '+' : additionner
-- Bouton '=' : calculer
+- Bouton 'Clear' : efface l'affichage en cours
+- Bouton 'History' : ouvre le menu déroulant contenant l'historique des calculs stocké dans le fichier 
+"**calcul_history.csv**"
+- Menu déroulant : affiche la liste de tous les calculs effectués
 
 ## Livrables attendus :
 
 - Un lien Github vers l’application
 
-- Une feuille comportant 5 tests fonctionnels de sa propre application
+- Une feuille comportant 10 tests fonctionnels de sa propre application
 
-- Une feuille comportant 5 tests fonctionnels d’une application concurrente.
+- Une feuille comportant 10 tests fonctionnels d’une application concurrente.
 
 ## Documentation Tkinter : 
 https://docs.python.org/fr/3/library/tk.html
+
+## Durée : 
+7h
+
+## Licence : 
+Ce projet est sous licence MIT.
+
+## Auteur : 
+Pierre Marboutin
+
+## Date de rendu
+24/01/24
+
+
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
 import pandas as pd
-import ast
 
 # result = []
 result_history = []
@@ -11,23 +10,23 @@ path = "calcul_history.csv"
 def on_key_press(event):
     current_text = entry.get()
     try:
-        result = eval(current_text)
         if event.keysym == "Return":
+            result = eval(current_text)
             print(result)
             entry_result.delete(0, tk.END)
             entry_result.insert(0, str(result))
-
             entry.delete(0, tk.END)
             entry.insert(0, current_text)
             result_history.append({'CALCUL': current_text, 'RESULTAT': result})
             history_df = pd.DataFrame(result_history)
             history_df.to_csv(path, index=False)
+            label_error.config(text="...")
 
-    except (SyntaxError, ValueError) as e:
-        pass
+    except (SyntaxError, ValueError, NameError,ZeroDivisionError) as e:
+        label_error.config(text=str(e))
 
 
-def clear_char():
+def clear_char(event):
     current_text = entry.get()
     if current_text:
         entry.delete(len(current_text))
@@ -36,6 +35,7 @@ def clear_char():
 
 
 def clear_screen():
+    label_error.config(text="")
     entry.delete(0, tk.END)
     entry_result.delete(0,tk.END)
 
@@ -43,10 +43,8 @@ def clear_screen():
 def on_combobox_select(event, combobox, entry, entry_result, df):
     selected_calculation = combobox.get()
     selected_result = df.loc[df['CALCUL'] == selected_calculation, 'RESULTAT'].values[0]
-
     entry.delete(0, tk.END)
     entry.insert(0, selected_calculation)
-
     entry_result.delete(0, tk.END)
     entry_result.insert(0, selected_result)
 
@@ -54,11 +52,8 @@ def on_combobox_select(event, combobox, entry, entry_result, df):
 def display_history():
     df = pd.read_csv(path)
     selected_line = df['CALCUL'].tolist()
-
     combobox = ttk.Combobox(root, values=selected_line)
     combobox.grid(row=2, column=3, sticky="nsew")
-
-    # Utilisez une lambda pour passer les paramètres nécessaires à on_combobox_select
     combobox.bind("<<ComboboxSelected>>", lambda event: on_combobox_select(event, combobox, entry, entry_result, df))
 
 
@@ -86,7 +81,12 @@ if __name__ == "__main__":
     history = tk.Button(root, text="History", command=display_history)
     history.grid(row=2, column=1)
 
-    # Configure columns
+    # display error
+    label_error = tk.Label(root, text="...", pady=30)
+    label_error.grid(row=3, column=1)
+
+
+    # configure columns
     for i in range(3):
         root.grid_columnconfigure(i, weight=1)
 
